9ca3e6db0835c82cea7fdba2181d7c32f2f2344a
Blade23445 <mikewindham4@gmail.com>
2023-01-23 13:01:20 -0600

    "Yes is the thing

---
+++ generateImage.js
@@ -0,0 +1,47 @@
+const Canvas = require("canvas")
+const Discord = require("dicsord.js")
+const background = "https://imgur.com/a/PDbXH47"
+
+const dim = {
+    height: 675,
+    width: 1200,
+    margin: 50
+}
+const av = {
+    size: 256,
+    x: 480,
+    y: 170
+}
+
+const generateImage = async (member) => {
+    let username = member.user.username
+    let discrim = member.user.discriminator
+    let avatarURL = member.user.displayAvatarURL({fomrat: "png", dynamic: false, size: av.size})
+    
+    const canvas = canvas.createCanvas(dim.witdth, dim.height)
+    const ctx = canvas.getContext("2d")
+
+    // draw in the background
+    const backimg = await Canvas.loadImage(background)
+    ctx.drawImage(backimg, 0, 0)
+
+    // draw black tinted box
+    ctx.fillStyle = "rgba(0,0,0,0.8)"
+    ctx.fillRect(dim.margine, dim.margin, dim.width - 2 * dim.margin, dim.height - 2 * dim.margin)
+
+    const avimg = await Canvas.loadImage(avatarURL)
+    ctx.save()
+
+    ctx.beginPath()
+    ctx.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.clip()
+
+    ctx.drawImage(avimg, av.x, av.y)
+    ctx.restore()
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "Welcome.png")
+    return attachment
+}
+
+module.exports = generateImage
